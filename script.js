3ce901a62fc32efdf55def5bfe5535616321dc44
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-06-16 16:00:16 -0400

    Remove debug text

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ divContainer.appendChild(newDiv); */
 
 for (i = 1; i <= 256; i++) {
     let newDiv = document.createElement("div");
-    newDiv.textContent = `${i}`
     newDiv.setAttribute("class", "square");
     divContainer.appendChild(newDiv);
 }
