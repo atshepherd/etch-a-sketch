8460376af4533ebcb43e36751251fe6aec523652
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-06-16 15:10:40 -0400

    Create 256 divs

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let divContainer = document.querySelector(".container");
 
-let newDiv = document.createElement("div");
+/* let newDiv = document.createElement("div");
 newDiv.textContent = "Test";
-divContainer.appendChild(newDiv);
+divContainer.appendChild(newDiv); */
+
+for (i = 1; i <= 256; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.textContent = `div #${i}`
+    divContainer.appendChild(newDiv);
+}
