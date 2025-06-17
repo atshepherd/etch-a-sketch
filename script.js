6204bbbc3356bb452b6ea36667bfcd74f47391c0
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-06-17 13:31:58 -0400

    Remove comment block

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let divContainer = document.querySelector(".container");
 
-/* let newDiv = document.createElement("div");
-newDiv.textContent = "Test";
-divContainer.appendChild(newDiv); */
-
 for (i = 1; i <= 256; i++) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "square");
